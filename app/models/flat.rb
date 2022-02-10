6491e5cfeab331e369cab2e93452203101750ffd
Anais Augu <augu.anais@gmail.com>
2022-02-10 21:44:35 +0100

    index show new and create ok

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,4 +1,8 @@
 class Flat < ApplicationRecord
-  validates :name, :address, :description, :price_per_night, :number_of_guests, presence: true
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :description, presence: true
+  validates :price_per_night, presence: true
+  validates :number_of_guests, presence: true
   validates :number_of_guests, :price_per_night, numericality: true
 end
